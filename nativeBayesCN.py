f3f783ee8d4ec2d5d6e07fa0859ab806abfcc8c2
Assianc <azure334792@gmail.com>
2024-05-27 00:35:40 +0800

    Changes

--- nativeBayesCN.py
+++ nativeBayesCN.py
@@ -1,11 +1,13 @@
+from tqdm import tqdm
 import jieba
 import multiprocessing as mp
 from itertools import islice
-from tqdm import tqdm
+
 
 def load_stop_words():
     """
-    加载停用词列表
+    加载停用词
+    :return:
     """
     stop_words = set()
     with open('./data/cnsmss/stopWord.txt', 'r', encoding='utf-8') as file:
@@ -13,12 +15,16 @@ def load_stop_words():
             stop_words.add(line.strip())
     return stop_words
 
+
 def loadDataSet(stop_words, lines=5000):
     """
-    加载并预处理中文数据集
+    读取中文数据集
+
+    :return:
     """
-    postingList = []
-    classVec = []
+    postingList = []  # 存储文本
+    classVec = []  # 存储标签
+
     with open('./data/cnsmss/80w.txt', 'r', encoding='utf-8') as file:
         dataSet = [line.strip().split('\t') for line in islice(file, lines)]
 
@@ -31,21 +37,63 @@ def loadDataSet(stop_words, lines=5000):
             words = jieba.lcut(item[2], cut_all=False)
             postingList.append(words)
         except IndexError:
-            postingList.append('')
             # 空文本
             pass
 
+        # try:
+        #     words = jieba.lcut(item[2], cut_all=False)
+        #     # 去除停用词
+        #     for word in words:
+        #         if word in stop_words:
+        #             words.remove(word)
+        #     postingList.append(words)
+        # except IndexError:
+        #     # 空文本
+        #     pass
+
     return postingList, classVec
 
-def preprocess_doc(args):
+
+def createVocabList(dataSet):
     """
-    单个文档预处理函数，用于多进程调用
+    提取数据集中的单词列表
+
+    :param dataSet:
+    :return:
     """
-    doc, stop_words = args
-    return ' '.join(jieba.lcut(doc, cut_all=False) if isinstance(doc, str) else doc)  # 预处理文档并返回处理后的文本字符串
+    # 分割数据集以便多进程处理
+    num_processes = mp.cpu_count()  # 获取CPU核心数量
+    chunk_size = len(dataSet) // num_processes
+
+    # 将数据集分割成多个块，每个块大小为 chunk_size
+    chunks = [dataSet[i * chunk_size:(i + 1) * chunk_size] for i in range(num_processes)]
+    # 如果数据集不能被核心数量整除，则将剩余数据添加到最后一个块中
+    if len(dataSet) % num_processes != 0:
+        chunks.append(dataSet[num_processes * chunk_size:])
+
+    with mp.Pool(processes=num_processes) as pool:
+        # 使用 imap_unordered 并行处理每个数据块
+        results = list(
+            tqdm(pool.imap_unordered(vocab_process, chunks), total=len(chunks), desc='创建词汇表：')
+        )
+
+    # 将所有结果合并
+    vocabSet = set().union(*results)
+    return list(vocabSet)
+
+
+def vocab_process(chunk):
+    vocabSet = set()
+    for document in chunk:
+        vocabSet = vocabSet | set(document)
+    return vocabSet
+
+
+
+def main():
+    stop_words = load_stop_words()
+    print(stop_words)
+
 
 if __name__ == '__main__':
-    # 这里仅用于演示或测试nativeBayesCN模块的功能，实际应用中不需要这部分代码
-    stop_words = load_stop_words()
-    listOposts, listClasses = loadDataSet(stop_words)
-    print("Data loaded.")
+    main()
