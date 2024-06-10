0d54ac7612368350a6f3c9fcdcc9b0e539cc637b
zhoubinxin <binxin.zhou@outlook.com>
2024-06-10 15:04:33 +0800

    Final

--- main.py
+++ main.py
@@ -1,100 +1,81 @@
-import json
-
-import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, average_precision_score
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
+import matplotlib as mpl
 
-import naiveBayes as nb
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
+from naiveBayes import *
 
 
 def main():
-    # 加载数据集
-    docs, label = nb.loadDataSet()
-    # 创建词汇表
-    vocabList = nb.createVocabList(docs)
+    docs, label = loadDataSet()
 
-    # 构建词向量矩阵
-    trainMat = []
-    for inputSet in tqdm(docs, desc='构建词向量矩阵'):
-        trainMat.append(nb.setOfWords2Vec(vocabList, inputSet))
-    #     trainMat.append(nb.bagOfWords2VecMN(vocabList, inputSet))
-    # tfidf = nb.TFIDF(docs, vocabList)
-    # trainMat = tfidf.calc_tfidf()
+    # 划分数据集
+    X_train, X_test, y_train, y_test = train_test_split(docs, label, test_size=0.2, random_state=1)
 
-    # 将数据集划分为训练集和测试集
-    # test_size 表示测试集的比例
-    # random_state 表示随机数的种子，保证每次划分的数据集都是相同的
-    X_train, X_test, y_train, y_test = train_test_split(trainMat, label, test_size=0.2, random_state=1)
+    vectorizer = WordToVec()
+    vectorizer.fit_tfidf(docs)
+    # setWordToVec bagWordToVec tfidfWordToVec
+    X_train_vec = vectorizer.tfidfWordToVec(X_train)
+    X_test_vec = vectorizer.tfidfWordToVec(X_test)
 
-    min_alpha = 1
-    max_alpha = 10
-    best_f1 = 0
-    best_alpha = None
-    best_metrics = None
+    # 使用 SimpleGridSearchCV 进行超参数搜索
+    alphaList = [1, 1.5, 2, 2.2, 2.3, 2.4, 2.5]
+    grid_search = ParamSearchCV(NaiveBayes(), alphaList, cv=5)
+    grid_search.fit(X_train_vec, y_train)
+    # 得到最佳参数
+    best_params = grid_search.best_params
+    print(f"最佳参数: {best_params}")
 
-    for alpha in np.arange(min_alpha, max_alpha, 0.1):
-        p0V, p1V, pAb = nb.trainNB0(X_train, y_train, alpha)
-        accuracy, precision, recall, f1, conf_matrix = nb.evaluate_model(p0V, p1V, pAb, X_test, y_test)
+    # 使用最佳参数训练模型
+    # best_model = SimpleNaiveBayes(3.0)
+    best_model = NaiveBayes(best_params)
+    best_model.fit(X_train_vec, y_train)
 
-        print(f"Alpha: {alpha}")
-        print(f"准确率Accuracy: {accuracy}")
-        print(f"精确率Precision: {precision}")
-        print(f"召回率Recall: {recall}")
-        print(f"F1 Score: {f1}")
-        print(f"Confusion Matrix:\n{conf_matrix}\n")
+    # 预测
+    y_pred = best_model.predict(X_test_vec)
 
-        if f1 > best_f1:
-            best_f1 = f1
-            best_alpha = alpha
-            best_metrics = (accuracy, precision, recall, f1, conf_matrix)
+    # 评估模型
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted', zero_division=1)
+    recall = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    conf_matrix = confusion_matrix(y_test, y_pred)
 
-    print(f"Best Alpha: {best_alpha}")
-    print(f"""
-    Best Metrics
-    Accuracy: {best_metrics[0]}
-    Precision: {best_metrics[1]}
-    Recall: {best_metrics[2]}
-    F1 Score: {best_metrics[3]}
-    Confusion Matrix: \n{best_metrics[4]}
-    """)
+    print(f"准确率: {accuracy}")
+    print(f"精确率: {precision}")
+    print(f"召回率: {recall}")
+    print(f"F1 值: {f1}")
+    print(f"混淆矩阵\n{conf_matrix}")
 
-    # 训练最佳模型
-    p0V, p1V, pAb = nb.trainNB0(trainMat, label, best_alpha)
+    # 输出结果
+    with open('result/best_score.txt', 'w', encoding='utf-8') as file:
+        file.write(f"最佳参数: {best_params}\n")
+        file.write(f"准确率: {accuracy}\n")
+        file.write(f"精确率: {precision}\n")
+        file.write(f"召回率: {recall}\n")
+        file.write(f"F1值: {f1}\n")
 
-    result = {
-        "vocabList": vocabList,
-        "p0V": p0V.tolist(),
-        "p1V": p1V.tolist(),
-        "pAb": pAb,
-    }
-    # 保存结果到json文件
-    with open('result/result.json', 'w', encoding='utf-8') as file:
-        json.dump(result, file, ensure_ascii=False)
+    # 设置中文字体
+    mpl.rcParams['font.sans-serif'] = ['SimHei']  # 使用黑体
+    mpl.rcParams['axes.unicode_minus'] = False  # 解决坐标轴负号显示问题
 
-    # 保存数据到txt
-    with open('result/score.txt', 'w', encoding='utf-8') as file:
-        # 评估指标
-        # accuracy, precision, recall, f1
-        file.write("\n评估指标:\n")
-        file.write(f'accuracy: {best_metrics[0]}\n')
-        file.write(f'precision: {best_metrics[1]}\n')
-        file.write(f'recall: {best_metrics[2]}\n')
-        file.write(f'f1: {best_metrics[3]}\n')
+    # 计算Precision-Recall曲线和平均精确率
+    precision, recall, thresholds = precision_recall_curve(y_test, y_pred)
+    average_precision = average_precision_score(y_test, y_pred)
 
-        # 混淆矩阵
-        file.write("\nconf_matrix:\n")
-        file.write(str(best_metrics[4]) + '\n')
+    # 绘制Precision-Recall曲线
+    plt.figure()
+    plt.step(recall, precision, where='post', color='b', alpha=0.7,
+             label='Precision-Recall曲线 (AP = %0.2f)' % average_precision)
+    plt.fill_between(recall, precision, step='post', alpha=0.3, color='b')
 
-    # 创建词云对象
-    # wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(vocabList)
-
-    # 显示词云
-    # plt.figure()
-    # plt.imshow(wordcloud, interpolation="bilinear")
-    # plt.axis("off")
-    # plt.show()
+    plt.xlabel('召回率')
+    plt.ylabel('精确率')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('Precision-Recall 曲线')
+    plt.legend(loc="lower left")
+    plt.show()
 
 
 if __name__ == '__main__':
